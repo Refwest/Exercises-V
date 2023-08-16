cc527fac3097c893f6035f8199f558d56b50e455
Filip Działa <fi.dziala@gmail.com>
2023-08-16 23:54:01 +0200

    Methods: Exercise 2 - square and cube of number

--- Program.cs
+++ Program.cs
@@ -13,23 +13,51 @@ namespace ExercisesV;
 class Program
 {
     //// !Exercise 1 - Multiplication *2 and *10
-static void Main(string[] args)
-{
-        Console.WriteLine("Enter your number");
-        double number = double.Parse(Console.ReadLine());
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Enter your number");
+    //    double number = double.Parse(Console.ReadLine());
 
-        MultiplicationTable(number);
+    //    MultiplicationTable(number);
+
+    //}
+    //public static void MultiplicationTable(double number)
+    //{
+    //    double multiplication = 0;
+    //    for (int i = 2; i <= 10; i++)
+    //    {
+    //        multiplication = number * i;
+    //        Console.WriteLine("{0,2} * {1,2} = {2,2}",number, i, multiplication);
+    //    }
+
+
+    //}
+
+
+    //// !Exercise 1 - Multiplication *2 and *10
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write a number which will be squared and cubed");
+        int number = int.Parse(Console.ReadLine());
+        
+        
+        Console.WriteLine("Number {0} squared is {1}", number,  Square(number));
+        Console.WriteLine("Number {0} cubedd is {1}", Cube(number));
     
     }
-    public static void MultiplicationTable(double number)
+
+    public static int Square(int numbers)
     {
-        double multiplication = 0;
-        for (int i = 2; i <= 10; i++)
-        {
-            multiplication = number * i;
-            Console.WriteLine("{0} * {1} = {2}",number, i, multiplication);
-        }
-       
-       
+        int squareNumber = numbers * numbers;
+
+        return squareNumber;
     }
+
+    public static int Cube (int number)
+    {
+        int cubeNumber = Square(number) * number;
+        
+        return cubeNumber;
+    }
+
 }
